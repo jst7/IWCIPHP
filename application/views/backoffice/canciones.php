de53c06ee72425ff6f5d694c1588670fc09c8b19
Jorge Segovia Tormo <jorge.segovia.tormo@gmail.com>
2017-01-26 14:10:57 +0100

    subir archivo canción

--- application/views/backoffice/canciones.php
+++ application/views/backoffice/canciones.php
@@ -8,7 +8,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<!-- Contenido principal -->
 	<main class="container">
 		<h1>Canciones</h1>
-		<div><?php echo $output; ?></div>
+		<div class="row">
+			<div><?php echo $output; ?></div>
+		</div>
+		<div class="row">
+			<form action="respuesta/<script.php" method="POST" name="f1" id="f1" role="form">
+				<div class="row">
+			    	<div class= "col-xs-6">
+			        	<div class="form-group">
+			            <label class="form-label" for="song"> Cancion: </label> 
+			            <input class="form-control" type="file" name="song" id="song" required size="25" maxlength="200" placeholder="Cancion" />
+			            </div>
+			        </div>
+			    </div>
+			    <div class="row">
+	        		<div class="col-xs-2">
+	        			<a class="btn btn-primary" type="submit" value="Enviar" id="btnEnviar"> Enviar </a>
+	        		</div>
+	        	</div>  
+			</form>
+		</div>
 
 	</main>
 
